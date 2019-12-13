502233fff674905d4be27d3eafcd13b604c3b964
Matt Collins <matt.collins@datum360.com>
2019-12-13 13:45:23 +0000

    working tests

--- build/lib/Person.test.js
+++ build/lib/Person.test.js
@@ -5,8 +5,4 @@ test('Expect person to initialise with name Matt', () => {
     const person = new Person_1.default('Matt');
     expect(person.name).toBe('Matt');
 });
-test('Expect person to initialise with name Steve', () => {
-    const person = new Person_1.default('Matt');
-    expect(person.name).toBe('Steve');
-});
 //# sourceMappingURL=Person.test.js.map
--- src/lib/Person.test.ts
+++ src/lib/Person.test.ts
@@ -6,10 +6,3 @@ test('Expect person to initialise with name Matt', () => {
   expect(person.name).toBe('Matt')
 
 })
-
-test('Expect person to initialise with name Steve', () => {
-
-  const person = new Person('Matt')
-  expect(person.name).toBe('Steve')
-
-})
